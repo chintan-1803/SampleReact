1614a2e91e6a1a0bc95d166bed011f9fc620acf5
chintan.dave <chintan.dave@spec-india.com>
2021-03-31 12:20:57 +0530

    Added Dialog to view youtube videos in the pop up

---
+++ src/Dialog.js
@@ -0,0 +1,45 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Button from "@material-ui/core/Button";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import Dialog from "@material-ui/core/Dialog";
+import { blue } from "@material-ui/core/colors";
+import { makeStyles } from "@material-ui/core/styles";
+
+function SimpleDialog(props) {
+  const { open, onClose } = props;
+
+  const handleClose = () => {
+    onClose();
+  };
+
+  return (
+    <Dialog
+      onClose={handleClose}
+      aria-labelledby="simple-dialog-title"
+      open={open}
+    >
+      <DialogTitle id="simple-dialog-title">Hit on Play Button</DialogTitle>
+      <video width="400" controls>
+        <source
+          src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
+          type="video/mp4"
+        />
+      </video>
+    </Dialog>
+  );
+}
+
+SimpleDialog.propTypes = {
+  onClose: PropTypes.func.isRequired,
+  open: PropTypes.bool.isRequired,
+};
+
+export default function SimpleDialogDemo(props) {
+  const { open, onClose } = props;
+  return (
+    <div>
+      <SimpleDialog open={open} onClose={onClose} />
+    </div>
+  );
+}
--- src/gridlist.js
+++ src/gridlist.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import GridList from '@material-ui/core/GridList';
-import GridListTile from '@material-ui/core/GridListTile';
-import GridListTileBar from '@material-ui/core/GridListTileBar';
-import ListSubheader from '@material-ui/core/ListSubheader';
-import IconButton from '@material-ui/core/IconButton';
-import InfoIcon from '@material-ui/icons/Info';
-
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import GridList from "@material-ui/core/GridList";
+import GridListTile from "@material-ui/core/GridListTile";
+import GridListTileBar from "@material-ui/core/GridListTileBar";
+import ListSubheader from "@material-ui/core/ListSubheader";
+import IconButton from "@material-ui/core/IconButton";
+import InfoIcon from "@material-ui/icons/Info";
+import Dialog from "./Dialog";
 //import tileData from './tileData';
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    display: 'flex',
-    flexWrap: 'wrap',
-    justifyContent: 'space-around',
-    overflow: 'hidden',
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "space-around",
+    overflow: "hidden",
     backgroundColor: theme.palette.background.paper,
   },
   gridList: {
@@ -22,7 +22,7 @@ const useStyles = makeStyles((theme) => ({
     height: 900,
   },
   icon: {
-    color: 'rgba(255, 255, 255, 0.54)',
+    color: "rgba(255, 255, 255, 0.54)",
   },
 }));
 
@@ -44,87 +44,105 @@ const useStyles = makeStyles((theme) => ({
  * ];
  */
 const tileData = [
-      {
-         img: '/images/English Vocabulary_26122020.png',
-         title: 'IELTS 7+ Band Vocabulary',
-         author: 'Chintan Dave',
-         playlist:'IELTS 7+ Band Vocabulary'
-       },
-       {
-         img: '/images/English Vocabulary_26122020.png',
-         title: 'IELTS 7+ Band Vocabulary',
-         author: 'Chintan Dave',
-         playlist:'IELTS 7+ Band Vocabulary'
-       },
-       {
-        img: '/images/English Vocabulary_26122020.png',
-        title: 'IELTS 7+ Band Vocabulary',
-        author: 'Chintan Dave',
-        playlist:'IELTS 7+ Band Vocabulary'
-      },
-      {
-        img: '/images/English Vocabulary_26122020.png',
-        title: 'IELTS 7+ Band Vocabulary',
-        author: 'Chintan Dave',
-        playlist:'IELTS 7+ Band Vocabulary'
-      },
-      {
-        img: '/images/English Vocabulary_26122020.png',
-        title: 'IELTS Task 2',
-        author: 'Chintan Dave',
-        playlist:'IELTS Task 2'
-      },
-      {
-        img: '/images/English Vocabulary_26122020.png',
-        title: 'IELTS Task 2',
-        author: 'Chintan Dave',
-        playlist:'IELTS Task 2'
-      },
-      {
-       img: '/images/English Vocabulary_26122020.png',
-       title: 'IELTS Task 2',
-       author: 'Chintan Dave',
-       playlist:'IELTS Task 2'
-     },
-     {
-       img: '/images/English Vocabulary_26122020.png',
-       title: 'IELTS Task 2',
-       author: 'Chintan Dave',
-       playlist:'IELTS Task 2'
-     }
-];
-export default function TitlebarGridList(props) {
-  const classes = useStyles();
-  if(props.test !== '')
   {
-  return (
-    <div className={classes.root}>
-      <GridList cellHeight={300} className={classes.gridList}>
-        <GridListTile key="Subheader" cols={2} style={{ height: 'auto' }}>
-          <ListSubheader component="div">All Videos</ListSubheader>
-        </GridListTile>
-        {tileData.filter(obj => obj.playlist == props.test).map((tile) => (
-          <GridListTile key={tile.img}>
-            <img src={tile.img} alt={tile.title} />
-            <GridListTileBar
-              title={tile.title}
-              subtitle={<span>by: {tile.author}</span>}
-              actionIcon={
-                <IconButton aria-label={`info about ${tile.title}`} className={classes.icon}>
-                  <InfoIcon />
-                </IconButton>
-              }
-            />
-          </GridListTile>
-           ))}
-      </GridList>
-    </div>
-  );
-}
-else{
-    return( <div className={classes.root}>
-            'No Content'
-    </div>);
-}
+    img: "/images/English Vocabulary_26122020.png",
+    title: "IELTS 7+ Band Vocabulary",
+    author: "Chintan Dave",
+    playlist: "IELTS 7+ Band Vocabulary",
+  },
+  {
+    img: "/images/English Vocabulary_26122020.png",
+    title: "IELTS 7+ Band Vocabulary",
+    author: "Chintan Dave",
+    playlist: "IELTS 7+ Band Vocabulary",
+  },
+  {
+    img: "/images/English Vocabulary_26122020.png",
+    title: "IELTS 7+ Band Vocabulary",
+    author: "Chintan Dave",
+    playlist: "IELTS 7+ Band Vocabulary",
+  },
+  {
+    img: "/images/English Vocabulary_26122020.png",
+    title: "IELTS 7+ Band Vocabulary",
+    author: "Chintan Dave",
+    playlist: "IELTS 7+ Band Vocabulary",
+  },
+  {
+    img: "/images/English Vocabulary_26122020.png",
+    title: "IELTS Task 2",
+    author: "Chintan Dave",
+    playlist: "IELTS Task 2",
+  },
+  {
+    img: "/images/English Vocabulary_26122020.png",
+    title: "IELTS Task 2",
+    author: "Chintan Dave",
+    playlist: "IELTS Task 2",
+  },
+  {
+    img: "/images/English Vocabulary_26122020.png",
+    title: "IELTS Task 2",
+    author: "Chintan Dave",
+    playlist: "IELTS Task 2",
+  },
+  {
+    img: "/images/English Vocabulary_26122020.png",
+    title: "IELTS Task 2",
+    author: "Chintan Dave",
+    playlist: "IELTS Task 2",
+  },
+];
 
+export default function TitlebarGridList(props) {
+  const [open, setOpen] = React.useState(false);
+
+  const setImageClick = () => {
+    // alert("hi");
+    handleClickOpen();
+    setOpen(true);
+    //return <SimpleDialog open={open} onClose={handleClose} />;
+  };
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = (value) => {
+    setOpen(false);
+  };
+  const classes = useStyles();
+  if (props.test !== "") {
+    return (
+      <div className={classes.root}>
+        <GridList cellHeight={300} className={classes.gridList}>
+          <GridListTile key="Subheader" cols={2} style={{ height: "auto" }}>
+            <ListSubheader component="div">All Videos</ListSubheader>
+          </GridListTile>
+          {tileData
+            .filter((obj) => obj.playlist == props.test)
+            .map((tile) => (
+              <GridListTile key={tile.img}>
+                <img src={tile.img} alt={tile.title} onClick={setImageClick} />
+                <GridListTileBar
+                  title={tile.title}
+                  subtitle={<span>by: {tile.author}</span>}
+                  actionIcon={
+                    <IconButton
+                      aria-label={`info about ${tile.title}`}
+                      className={classes.icon}
+                    >
+                      <InfoIcon />
+                    </IconButton>
+                  }
+                />
+              </GridListTile>
+            ))}
+        </GridList>
+        <Dialog open={open} onClose={handleClose} />
+      </div>
+    );
+  } else {
+    return <div className={classes.root}>'No Content'</div>;
+  }
 }
